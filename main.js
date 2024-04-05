01fcee2f63ef88a0b3cde44d160a4a43eb1ccb69
syafiqah3012 <syafiqahizani@gmail.com>
2024-04-05 13:19:19 +0800

    update

--- main.js
+++ main.js
@@ -544,18 +544,20 @@ function addHelixObject(helixVector1, helixVector2, index) {
 
     object.position.setFromCylindricalCoords(900, theta, y);
 
-    const distance = 0.1; // Adjust this distance to bring helixVector1 and helixVector2 extremely close
+    const grooveWidth = 5; // Width of the groove
+    const distance = 10; // Distance between major and minor grooves
 
     if (index % 4 < 2) { // Alternate between major and minor grooves
-        helixVector1.copy(object.position).add(new THREE.Vector3(distance, 0, 0));
+        helixVector1.copy(object.position).add(new THREE.Vector3(0, grooveWidth / 2, 0));
         object.lookAt(helixVector1);
     } else {
-        helixVector2.copy(object.position).add(new THREE.Vector3(-distance, 0, 0));
+        helixVector2.copy(object.position).add(new THREE.Vector3(0, -grooveWidth / 2, distance));
         object.lookAt(helixVector2);
     }
 
     targets.helix.push(object);
 }
+
 
 
 
